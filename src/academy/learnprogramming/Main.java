5abaea65f268795904c319208eada6def2f5d070
ryoray <praretno.wibowo@gmail.com>
2020-07-18 09:22:19 +0700

    Verified Fixed

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -1,30 +1,48 @@
 package academy.learnprogramming;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number;
+        System.out.print("How many number : ");
+        number = scanner.nextInt();
 
+        System.out.println("----------------------------");
+
+        int[] array = new int[number];
+        for (int i=0; i<array.length; i++) {
+            int input = scanner.nextInt();
+            scanner.nextLine();
+            array[i] = input;
+        }
+
+        System.out.println("----------------------------");
+
+        for (int i=0; i<array.length; i++) {
+            System.out.println("Element["+ i + "] = " + array[i]);
+        }
+
+        int[] reversedArray = reverse(array);
+
+        System.out.println("----------------------------");
+        for (int i=0; i<reversedArray.length; i++) {
+            System.out.println("Element[" + i + "] = " + reversedArray[i]);
+        }
     }
 
-    private static void reverse(int[] array) {
+    private static int[] reverse(int[] array) {
         int[] arrayToReverse = array;
         int[] arrayReversed = new int[arrayToReverse.length];
-        int elementToSwapFirst;
-        int elementToSwapLast;
-        int temp;
-        int FirstElementAfterSwap;
-        int LastElementAfterSwap;
 
-        for (int i=0; i<(arrayToReverse.length/2); i++ ) {
-            elementToSwapFirst = arrayToReverse[i];
-            elementToSwapLast = arrayToReverse[arrayReversed.length-1];
-            
-            temp = elementToSwapFirst;
-            elementToSwapFirst = elementToSwapLast;
-            elementToSwapLast = temp;
+        arrayReversed[0] = arrayToReverse[arrayToReverse.length-1];
 
-            arrayReversed[i] = elementToSwapFirst;
-            arrayReversed[arrayReversed.length-i] = elementToSwapLast;
+        for (int i=1; i<arrayToReverse.length; i++ ) {
+            int element = arrayToReverse.length-(i+1);
+            arrayReversed[i] = arrayToReverse[element];
         }
+        return arrayReversed;
     }
 }
